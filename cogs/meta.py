35fc5d57919104e007554313dbbab76f5f74e9bb
EraseKesu <eitan.olchik@gmail.com>
2020-04-01 13:52:32 +0100

    updated meta.py

--- cogs/meta.py
+++ cogs/meta.py
@@ -12,6 +12,29 @@ class Meta(commands.Cog):
             title="Source",
             description="https://github.com/EraseKesu/Chorus"
         )
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def feedback(self, ctx, *feedback):
+        msg = ' '.join(feedback)
+
+        embed = discord.Embed(
+            title="Feedback",
+            description=f"{msg} \n~\n   ctx.author",
+            colour=0x0EF7E2
+        )
+        emb = discord.Embed(
+            title="Thanks For Your Feedback!",
+            description="Thanks for your feedback! We will try to improve as soon as possible!",
+            colour=0x0EF7E2
+        )
+        emb.set_footer(
+            text="Join our support server! `https://discord.gg/YUm2sBD`",
+            icon_url=self.bot.user.avatar_url_as(static_format="png")
+        )
+
+        channel = await self.bot.fetch_channel(694887120669507635)
+        await channel.send(embed=embed)
 
 
 def setup(bot):
